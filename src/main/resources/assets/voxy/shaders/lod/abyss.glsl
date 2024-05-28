639dcec6bcc906b8e4bfdfae437e1ae62a357b5e
semyon422 <semyon422@users.noreply.github.com>
2024-05-28 18:48:40 +0500

    Fix culling
    (cherry picked from commit eb8e52044dacf2f8ae2d0f482de6a4263e1b7238)

--- src/main/resources/assets/voxy/shaders/lod/abyss.glsl
+++ src/main/resources/assets/voxy/shaders/lod/abyss.glsl
@@ -1,14 +1,18 @@
-vec3 abyss_offset(vec3 pos, uint lvl, int lod_y) {
-    if (lod_y >= 0) return pos;
-
-    int delta = 16 >> lvl;
+int abyss_offset32(uint lvl, int lod_y) {
     int start = -8 >> lvl;
+    if (lod_y >= start) return 0;
+    int delta = 16 >> lvl;
+    return (start - lod_y + delta - 1) / delta;
+}
 
+vec3 abyss_offset(vec3 pos, uint lvl, int lod_y) {
     vec3 ret = vec3(pos);
-    for (int i = 0; i < 16; i++) {
-        if (lod_y < start - delta * i) {
-            ret.y = ret.y + 32;
-        }
-    }
+    ret.y = ret.y + 32 * abyss_offset32(lvl, lod_y);
+    return ret;
+}
+
+ivec3 abyss_offset32(ivec3 baseSectionPos, uint lvl, int lod_y) {
+    ivec3 ret = ivec3(baseSectionPos);
+    ret.y = ret.y - abyss_offset32(lvl, lod_y);
     return ret;
 }
